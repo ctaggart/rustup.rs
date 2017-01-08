f85b5a1d8a9288bb2581df425944d84fe72452ee
Cameron Taggart <cameron@tachyus.com>
2017-01-07 20:48:36 -0800

    chocolatey package for rustup

---
+++ build.bat
@@ -0,0 +1,3 @@
+touch paket.dependencies
+touch paket.lock
+paket pack version 1.0.0-beta1 output packages
---
+++ chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'rustup-init'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://win.rustup.rs/?version=1.0.0'
+
+# Get-FileHash ~\rustup-init.exe
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  validExitCodes= @(0)
+
+  softwareName  = 'rustup-init*'
+  checksum      = '7AFD5E148BDF15AA9B46CB1B3B857AF1F458853287069A0C9C7ADAD541C3B54C'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
